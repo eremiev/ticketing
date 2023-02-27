314ceb6432f5a71dfc12b5d2dc652f92ca456596
Emil <emil.eremiev@capex.tech>
2023-02-27 22:47:41 +0200

    add new listener for expiration completed for order service

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,30 @@
+import {Message} from 'node-nats-streaming';
+import {Listener, Subjects, ExpirationCompleteEvent, OrderStatus} from '@eremiev/common';
+import {queueGroupName} from './queue-group-name';
+import {OrderCancelledPublisher} from '../publishers/order-cancelled-publisher';
+import {Order} from '../../models/order';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    readonly subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const {orderId} = data;
+        const order = await Order.findById(orderId).populate('ticket');
+        if (!order) {
+            throw new Error('Order not found');
+        }
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+        await order.save();
+        await new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+        msg.ack();
+    }
+}
