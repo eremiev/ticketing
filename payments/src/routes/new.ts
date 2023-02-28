1329b264039b15bcb4fea4c5b957cce038d3b8c5
Emil <emil.eremiev@capex.tech>
2023-02-28 22:33:31 +0200

    add payment publisher in payment service and add payment created listener in order service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,9 @@ import {
 } from '@eremiev/common';
 import {stripe} from '../stripe';
 import {Order} from '../models/order';
+import {Payment} from '../models/payments';
+import {PaymentCreatedPublisher} from '../events/publishers/payment-created-publisher';
+import {natsWrapper} from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,7 +26,6 @@ router.post('/api/payments',
     async (req: Request, res: Response) => {
         const {token, orderId} = req.body;
         const order = await Order.findById(orderId);
-
         if (!order) {
             throw new NotFoundError();
         }
@@ -34,13 +36,23 @@ router.post('/api/payments',
             throw new BadRequestError('Cannot pay for cancelled order');
         }
 
-        await stripe.charges.create({
+        const charge = await stripe.charges.create({
             currency: 'usd',
             amount: order.price * 100,
             source: token
         });
+        const payment = Payment.build({
+            orderId,
+            stripeId: charge.id
+        });
+        await payment.save();
+        await new PaymentCreatedPublisher(natsWrapper.client).publish({
+            id: payment.id,
+            orderId: payment.orderId,
+            paymentId: payment.stripeId
+        });
 
-        res.status(201).send({success: true});
+        res.status(201).send({id: payment.id});
     });
 
 export {router as createChargeRouter};
